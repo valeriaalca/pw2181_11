d905e022f8c81eeb7cc9f3fba171eecff72390c2
ALAPIZCO ACOSTA JESHUA GILDARDO <13170210@itculiacan.edu.mx>
2018-04-24 11:58:12 -0600

    Primer JQuery

---
+++ js/eventosjquery.js
@@ -0,0 +1,29 @@
+var inicia = function()
+
+{
+	var nuevo = function ()
+	{
+		//JSON=JavaScript Object Notation
+		//alert("clic");
+		$.ajax({
+  		url: 'https://randomuser.me/api/',
+  		dataType: 'json',
+  		success: function(data) 
+  		{
+  			$("#nombre").html(data.results[0].name.first+" "+data.results[0].name.last);
+  			$("#foto").attr( "src", data.results[0].picture.large);
+  			
+
+
+    	//console.log(data);
+  }
+});
+    
+	}
+	$("#btnNuevo").on("click", nuevo)
+	//alert("Pagina Cargada");
+}
+
+
+
+$(document).ready(inicia);

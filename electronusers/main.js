712d0b1517096e7573c040dfd8c138fc1b9d56fc
ALAPIZCO ACOSTA JESHUA GILDARDO <13170210@itculiacan.edu.mx>
2018-04-30 11:59:19 -0600

    Primer Pantalla Electron

---
+++ electronusers/main.js
@@ -0,0 +1,27 @@
+
+//Cargamos la aplicacion de electron
+const app = require ('electron').app;
+
+//Para crear ventanas del sistema operativo
+const BrowserWindow=require('electron').BrowserWindow;
+const path=require('path');
+const url = require('url');
+
+//let es una constante igual que const 
+let PantallaPrincipal;
+
+
+function muestraPantallaPrincipal()
+{
+	//Creamos una pantalla vacia 
+	PantallaPrincipal=new BrowserWindow({width:620, height:825});
+	//Le damos contenido a esa pantalla
+	PantallaPrincipal.loadURL(url.format({
+		pathname:path.join(__dirname,'index.html'),
+		protocol: 'file',
+		slashes: true
+	}));
+	//Mostramos la pantalla
+	PantallaPrincipal.show();
+}
+app.on('ready', muestraPantallaPrincipal);
